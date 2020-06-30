365a9cb6580b227e15b316099b2e407901d0807b
Shakur Xussein <shakurxussein@Shakurs-MacBook-Pro.local>
2020-06-30 10:58:07 -0700

    defined class and class objects and methods

--- OOP/intro.py
+++ OOP/intro.py
@@ -6,3 +6,17 @@ class Sample():
 	def Some_Method(self):
 		# perform this action
 		print(self.param1)
+
+#===============================================
+
+class Dog():
+    # class object attributes
+    # same for any instance of a class
+    species = "mamal"
+
+    def __init__(self, breed, name, spots):
+        # assign aruguments to attributes via
+        # self attribute
+        self.breed = breed 
+        self.name = name
+        self.spots = spots # expecting a boolead in return
