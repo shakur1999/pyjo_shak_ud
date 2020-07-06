524df102dd2d843863aa6d49c2bbe1625d079036
shakur1999 <shakurxussein@Shakurs-MacBook-Pro.local>
2020-07-06 16:46:39 -0700

    test upload, no solution yet

---
+++ advanced_objects_test.py
@@ -0,0 +1,44 @@
+
+Advanced Python Objects Test
+Advanced Numbers
+Problem 1: Convert 1024 to binary and hexadecimal representation
+
+In [ ]:
+
+Problem 2: Round 5.23222 to two decimal places
+
+In [ ]:
+
+Advanced Strings
+Problem 3: Check if every letter in the string s is lower case
+
+In [ ]:
+s = 'hello how are you Mary, are you feeling okay?'
+Problem 4: How many times does the letter 'w' show up in the string below?
+
+In [ ]:
+s = 'twywywtwywbwhsjhwuwshshwuwwwjdjdid'
+Advanced Sets
+Problem 5: Find the elements in set1 that are not in set2:
+
+In [ ]:
+set1 = {2,3,1,5,6,8}
+set2 = {3,1,7,5,6,8}
+Problem 6: Find all elements that are in either set:
+
+In [ ]:
+
+Advanced Dictionaries
+Problem 7: Create this dictionary: {0: 0, 1: 1, 2: 8, 3: 27, 4: 64} using a dictionary comprehension.
+
+In [ ]:
+
+Advanced Lists
+Problem 8: Reverse the list below:
+
+In [ ]:
+list1 = [1,2,3,4]
+Problem 9: Sort the list below:
+
+In [ ]:
+list2 = [3,4,2,5,1]
