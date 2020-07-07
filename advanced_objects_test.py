973f787a499d1d1e6cdd144f0b99f3aabc98cb05
shakur1999 <ashakur1999@gmail.com>
2020-07-06 17:56:35 -0700

    draft solution

--- advanced_objects_test.py
+++ advanced_objects_test.py
@@ -2,25 +2,29 @@
 # Advanced Python Objects Tes Advanced Numbers
 
 #Problem 1: Convert 1024 to binary and hexadecimal representation
-
+bin(1024)
 
 # Problem 2: Round 5.23222 to two decimal places
-
+round(5.23222, 2)
 
 # Advanced Strings
 # Problem 3: Check if every letter in the string s is lower case
-
 s = 'hello how are you Mary, are you feeling okay?'
-# Problem 4: How many times does the letter 'w' show up in the string below?
+s.islower()
 
+# Problem 4: How many times does the letter 'w' show up in the string below?
 s = 'twywywtwywbwhsjhwuwshshwuwwwjdjdid'
+s.count("w")
+
 # Advanced Sets
 # Problem 5: Find the elements in set1 that are not in set2:
 
 set1 = {2,3,1,5,6,8}
 set2 = {3,1,7,5,6,8}
+set1.intersection(set2)
 
 # Problem 6: Find all elements that are in either set:
+set.symmetric_difference(set2)
 
 
 # Advanced Dictionaries
@@ -29,8 +33,10 @@ set2 = {3,1,7,5,6,8}
 
 # Advanced Lists
 # Problem 8: Reverse the list below:
-
 list1 = [1,2,3,4]
-# Problem 9: Sort the list below:
+list1.reverse()
 
+# Problem 9: Sort the list below:
 list2 = [3,4,2,5,1]
+list2.sort()
+
