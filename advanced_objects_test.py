190b8b82c95c279e7437ae9ed33b717eed995a8c
shakur1999 <ashakur1999@gmail.com>
2020-07-06 17:04:44 -0700

    no solution yet

--- advanced_objects_test.py
+++ advanced_objects_test.py
@@ -1,44 +1,36 @@
 
-Advanced Python Objects Test
-Advanced Numbers
-Problem 1: Convert 1024 to binary and hexadecimal representation
+# Advanced Python Objects Tes Advanced Numbers
 
-In [ ]:
+#Problem 1: Convert 1024 to binary and hexadecimal representation
 
-Problem 2: Round 5.23222 to two decimal places
 
-In [ ]:
+# Problem 2: Round 5.23222 to two decimal places
 
-Advanced Strings
-Problem 3: Check if every letter in the string s is lower case
 
-In [ ]:
+# Advanced Strings
+# Problem 3: Check if every letter in the string s is lower case
+
 s = 'hello how are you Mary, are you feeling okay?'
-Problem 4: How many times does the letter 'w' show up in the string below?
+# Problem 4: How many times does the letter 'w' show up in the string below?
 
-In [ ]:
 s = 'twywywtwywbwhsjhwuwshshwuwwwjdjdid'
-Advanced Sets
-Problem 5: Find the elements in set1 that are not in set2:
+# Advanced Sets
+# Problem 5: Find the elements in set1 that are not in set2:
 
-In [ ]:
 set1 = {2,3,1,5,6,8}
 set2 = {3,1,7,5,6,8}
-Problem 6: Find all elements that are in either set:
 
-In [ ]:
+# Problem 6: Find all elements that are in either set:
 
-Advanced Dictionaries
-Problem 7: Create this dictionary: {0: 0, 1: 1, 2: 8, 3: 27, 4: 64} using a dictionary comprehension.
 
-In [ ]:
+# Advanced Dictionaries
+# Problem 7: Create this dictionary: {0: 0, 1: 1, 2: 8, 3: 27, 4: 64} using a dictionary comprehension.
 
-Advanced Lists
-Problem 8: Reverse the list below:
 
-In [ ]:
+# Advanced Lists
+# Problem 8: Reverse the list below:
+
 list1 = [1,2,3,4]
-Problem 9: Sort the list below:
+# Problem 9: Sort the list below:
 
-In [ ]:
 list2 = [3,4,2,5,1]
